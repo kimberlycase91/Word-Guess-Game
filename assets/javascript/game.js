83e1598e2bea63fb2fcef1350943ececcfe8ffff
kimberlycase91 <kimberlycase91@gmail.com>
2019-12-16 20:10:26 -0700

    worked on blank space and correct guess reveal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,50 @@
+// document.addEventListener ('keydown') {
+
 //array of possibel words
-var word
-//number of wins
-var wins = getElementById("wins-div")
+var word = ["red", "blue", "black", "yellow"];
+var reveal;
+var guess;
+var alphabet = "abcdefghijjklmnopqrstuvwxyz".split("");
+var correctLetters = [];
+var lettersGuessed = [];
+var wins = 0;
+var revealArray = [];
+
+//choose a random word
+var word = word[Math.floor(Math.random() * word.length)];
+console.log(word);
+
 //blank spaces-letter reveal
-var reveal
-//number of guesses allowed
-var numberOfGuesses = 15;
-//number of guesses remaining
-var guessesRemaining
-//letters already guessed
-var lettersGuessed
+function randomWord () {
+for (var i=0; i < word.length; i++) {
+      revealArray[i] = "_";
+}
+document.getElementById("reveal-div").innerHTML = revealArray.join (" ");
+}
+console.log(revealArray);
 
-wins.document.write(text("hello"))
+// shows how many letters are left in the word
+var remainingLetters = word.length + 5;
+document.getElementById("guesses-remaining-div").innerHTML = remainingLetters;
+console.log(remainingLetters);
 
-//player presses any key to begin
+function guesses() {
+      document.onkeyup = function(event) {
+            guess = event.key.toLowerCase();
+            for (i = 0; i < word.length; i++) {
+                  if (guess === word[i]) {
+                        correctLetters[i] = guess;
+                        document.getElementById("reveal-div").innerHTML = correctLetters.join(" ");
+                        revealArray[i]="_";
+                        // return document.getElementById ("endgame-div").innerHTML = "You Win!";
+                  }
+            }
+      }
+}
 
-//computer chooses a word from var word array
 
-//computer counts how many letters are in that word and displays a "_" for each letter in the reveal
 
-//on keyup if its a letter, computer assesses if it is in the word
-
-      //if the letter is in the word, computer replaces the corresponding "_" in the reveal with the correct letter  AND decreases the number of guesses remaining by 1
-
-      //if it is not a letter, nothing happens?
-      //if the letter is not in the word, the computer adds the letter to the letters already guessed and decreases the number of guesses remaining by 1
-
-//
+document.onkeyup = function(event){
+randomWord();
+guesses();
+}
